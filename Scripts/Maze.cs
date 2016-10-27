37356b49f1dcbe7367ee21cbb0cc92ccd226ad19
zhangyn944 <zhangyn94@gmail.com>
2016-10-27 12:15:42 +0800

    Add files via upload

---
+++ Scripts/Maze.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour {
+
+	public static bool collectTheTreasure;
+	public static float timer;
+	public static float MaxTime = 60.0f;
+	public AudioClip enterAudio;
+	 
+	void Start(){
+		collectTheTreasure = false;
+		timer = 0.0f;
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.tag == "Player") {
+			AudioSource.PlayClipAtPoint (enterAudio, collider.transform.position);
+		}
+	}
+
+	void OnTriggerStay(Collider collider){
+		if (collider.gameObject.tag == "Player") {
+			timer += Time.deltaTime;
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+		if (collider.gameObject.tag == "Player") {
+			timer = 0.0f;
+		}
+	}
+
+	void Update(){ 
+		if (timer >= MaxTime) {
+			collectTheTreasure = false;
+		}
+		if (FindTheTreasure.isSuccess == true) {
+			collectTheTreasure = true;
+		}
+	}
+}
---
+++ Scripts/minimapCamFellow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class minimapCamFellow : MonoBehaviour {
+
+	public Transform target;
+		
+	void LateUpdate(){
+		transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z);
+	}
+}
