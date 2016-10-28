b9a8b3b37998a22c55966a64fb4c5d5deaf814f5
zhangyn944 <zhangyn94@gmail.com>
2016-10-28 16:27:35 +0800

    Add files via upload
    这是成为tribe B hero 的迷宫的脚本

---
+++ FindTheTreasure.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindTheTreasure : MonoBehaviour {
+	public static bool isSuccess;
+	public AudioClip success;
+	public Collider c;
+
+	void Start(){
+		isSuccess = false;
+		c = GetComponent<Collider> ();
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.tag == "Player" && Maze.timer <= Maze.MaxTime) {
+			isSuccess = true;
+			AudioSource.PlayClipAtPoint (success, collider.transform.position);
+		
+			c.enabled = false;			//禁用敌人的collider组件，使其不会与其他物体发生碰撞
+			Destroy (gameObject, 1.0f);			//1秒后删除敌人对象
+		}
+	}
+}
---
+++ Maze.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour {
+
+	public static bool collectTheTreasure;
+	public static float timer;
+	public static float MaxTime = 30.0f;
+	public AudioClip successAudio;
+	GameObject player;
+	 
+	void Start(){
+		collectTheTreasure = false;
+		timer = 0.0f;
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update(){
+		float currDis = Vector3.Distance (player.transform.position, transform.position);
+		Debug.Log ("current distance = " + currDis);
+
+		if ( currDis <= 50) {
+			Debug.Log ("inside the maze");
+
+			timer += Time.deltaTime;
+		}
+		if(currDis > 50) {
+			Debug.Log ("reenter the maze" );
+			timer = 0.0f;
+		}
+		if (timer >= MaxTime && FindTheTreasure.isSuccess == false) {
+			collectTheTreasure = false;
+			Debug.Log("fail task");
+		}
+		if (FindTheTreasure.isSuccess == true) {
+			collectTheTreasure = true;
+			Debug.Log("become Hero!!!");
+		}
+	}
+}
