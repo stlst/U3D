5af50736cdd1c4e3c64fc994e26af664b8a52680
Vico <kongwk@foxmail.com>
2016-10-29 11:54:06 +0800

    Add files via upload

--- Scripts_alice/CharacterDemoController.cs
+++ Scripts_alice/CharacterDemoController.cs
@@ -18,6 +18,12 @@ public class CharacterDemoController : MonoBehaviour
 	public int HP=100;
 	public int maxHP;
 	public int attackDamage;
+	public int[] drug_num;
+	public int drug_HP = 20;
+	public int drug_damage = 3;
+	public int drug_maxHP = 20;
+	private float drug_maxHP_timer = 0f;
+	private float drug_damage_timer = 0f;
 //	public int speed;
 	public float minAttackTime = 1.5f;
 	private float timer = 0.0f;	// record attack time duration
@@ -35,6 +41,7 @@ public class CharacterDemoController : MonoBehaviour
 	public Vector3		lookAtPos;
 	public Vector3      deltaTarget;
 	public int 			WeaponState=1;
+	TreasureBox treasureBox;
 	Ray ray;
 	RaycastHit hitInfo;
 	GameObject gameObj;
@@ -47,26 +54,30 @@ public class CharacterDemoController : MonoBehaviour
 		levelXP = new int[]{100,300,900,2700,99999999 };
 		levelHP = new int[]{100,105,110,115,120};
 		levelDamage = new int[]{8,9,10,11,12 };
-		heroList = new bool[3]{ false, false, false };
+		drug_num = new int[]{ 0, 0, 0 }; //the first drug is to cure player, the second is to add damage, the third is to add maxHP.
+		heroList = new bool[3]{ false, true, true };
 		animator = GetComponentInChildren<Animator>();//need this...
 		movementTargetPosition = transform.position;//initializing our movement target as our current position
 
 	}
 	
 	// Update is called once per frame
-	public void Update () 
+	public void Update ()
 	{
 		//The Update logic does:
 		//	Get UI input from keyboard, and mouse clicks
 		//	Tells mecanim what weaponstate we are in
 		//	Tells mecanim what animation we should be playing based on variables such as idling, pain or death
 		//	Handle movement and direction, apply root motion to move
+		if (GameManager.gameManager == null || GameManager.gameManager.gameState == GameManager.GameState.Playing) {
+			changeWeapon ();
+			characterMove ();
 
-		changeWeapon();
-		characterMove ();
-		isDead ();
-		
-
+	/*		if (drug_maxHP_timer > 0 || drug_damage_timer > 0) {
+				update_state ();
+			} */
+			eatDrug ();
+		}
 	}
 
 	public void characterMove(){
@@ -84,7 +95,7 @@ public class CharacterDemoController : MonoBehaviour
 			}
 			if (rightmouse == 0) {
 				if (gameObj.tag == "MonsterA") {
-					Debug.Log ("MonsterA");
+	//				Debug.Log ("MonsterA");
 					if (WeaponState != 2 && heroList [0] == false) {
 						minDist = 3f;
 						movementTargetPosition = gameObj.transform.position;
@@ -132,7 +143,7 @@ public class CharacterDemoController : MonoBehaviour
 			
 			if (rightmouse == 1) {
 				if (gameObj.tag == "MonsterA") {
-					Debug.Log ("MonsterA");
+					Debug.Log ("Attack MonsterA");
 					if (WeaponState != 2 && heroList [0] == false) {
 						minDist = 3.5f;
 						movementTargetPosition = gameObj.transform.position;
@@ -163,6 +174,12 @@ public class CharacterDemoController : MonoBehaviour
 					Debug.Log ("Boss");
 					minDist = 3.5f;
 					movementTargetPosition = gameObj.transform.position;
+				} else if (gameObj.tag == "Drug") {
+					Debug.Log ("Drug");
+					minDist = 2f;
+					movementTargetPosition = gameObj.transform.position;
+					treasureBox = gameObj.GetComponent<TreasureBox> ();
+					treasureBox.box_behaviour ();
 				}
 			}
 
@@ -196,7 +213,19 @@ public class CharacterDemoController : MonoBehaviour
 				timer = 0.0f;
 				attack ();
 
-
+			} else if (minDist == 2f) {
+				if (!treasureBox.isEmpty ()) {
+					treasureBox.distribution ();
+					System.Random rd = new System.Random ();
+					int randKey = rd.Next (1, 100);
+					if (0 < randKey && randKey <= 80)
+						drug_num [0]++;
+					else if (80 < randKey && randKey <= 90)
+						drug_num [1]++;
+					else
+						drug_num [2]++;
+				}
+				timer += Time.deltaTime;
 			} else {
 				timer += Time.deltaTime;
 			}
@@ -224,14 +253,115 @@ public class CharacterDemoController : MonoBehaviour
 		default:
 			break;
 		}
-		changeState ();
+		update_state ();
 		animator.SetInteger("WeaponState", WeaponState);// probably would be better to check for change rather than bashing the value in like this
 	}
 
+	public void eatDrug(){
+		switch(Input.inputString)//get keyboard input, probably not a good idea to use strings here...Garbage collection problems with regards to local string usage are known to happen
+		{						 //the garbage collection memory problem arises from local alloction of memory, and not freeing it up efficiently
+		case "q":
+			if (drug_num [0] > 0) {
+				Debug.Log ("HP:" + HP + "  maxHP:" + maxHP+"  damage: "+attackDamage);
+				if (HP >= maxHP)
+					break;
+				drug_num [0]--;
+				HP += drug_HP;
+
+				if (HP > maxHP)
+					HP = maxHP;
+				Debug.Log ("HP1:" + HP + "  maxHP1:" + maxHP+"  damage1: "+attackDamage);
+			}
+			break;
+		case "w":
+			if(drug_num [1] > 0){
+				drug_num [1]--;
+				drug_maxHP_timer = 120f;
+			}
+				
+			break;
+		case "e":
+			if(drug_num [2] > 0){
+				drug_num [2]--;
+				drug_damage_timer = 120f;
+			}
+			break;	
+		default:
+			break;
+		}
+	}
+	void update_state(){
+		if (drug_maxHP_timer > 0) {
+			if (WeaponState == 1) {
+				maxHP = levelHP [level] + drug_maxHP;
+				if (HP > maxHP)
+					HP = maxHP;
+			} else if (WeaponState == 2) {
+				maxHP = levelHP [level] + 20 +drug_maxHP;
+				if (HP > maxHP)
+					HP = maxHP;
+			} else if (WeaponState == 4) {
+				maxHP = levelHP [level] + drug_maxHP;
+				if (HP > maxHP)
+					HP = maxHP;
+			} else if (WeaponState == 7) {
+				maxHP = levelHP [level] + drug_maxHP;
+				if (HP > maxHP)
+					HP = maxHP;
+			}
+		}
+		drug_maxHP_timer -= Time.deltaTime;
+		if (drug_maxHP_timer <= 0) {
+			if (WeaponState == 1) {
+				maxHP = levelHP [level];
+				if (HP > maxHP)
+					HP = maxHP;
+			} else if (WeaponState == 2) {
+				maxHP = levelHP [level] + 20;
+				if (HP > maxHP)
+					HP = maxHP;
+			} else if (WeaponState == 4) {
+				maxHP = levelHP [level];
+				if (HP > maxHP)
+					HP = maxHP;
+			} else if (WeaponState == 7) {
+				maxHP = levelHP [level];
+				if (HP > maxHP)
+					HP = maxHP;
+			}
+		}
+
+		if (drug_damage_timer > 0) {
+			if (WeaponState == 1) {
+				attackDamage = levelDamage [level] + drug_damage;
+			} else if (WeaponState == 2) {
+				attackDamage = levelDamage [level] + 1 +drug_damage;
+			} else if (WeaponState == 4) {
+				attackDamage = levelDamage [level] + 1 +drug_damage;
+			} else if (WeaponState == 7) {
+				attackDamage = levelDamage [level] + 3 +drug_damage;
+			}
+		}
+		drug_damage_timer -= Time.deltaTime;
+		if (drug_damage_timer <= 0) {
+			if (WeaponState == 1) {
+				attackDamage = levelDamage [level];
+			} else if (WeaponState == 2) {
+				attackDamage = levelDamage [level] + 1;
+			} else if (WeaponState == 4) {
+				attackDamage = levelDamage [level] + 1;
+			} else if (WeaponState == 7) {
+				attackDamage = levelDamage [level] + 3;
+			}
+		}
+	}
+
 	public void attack(){
+		if (HP <= 0)
+			return;
 		gameObj = hitInfo.collider.gameObject;
 		if (gameObj.tag == "MonsterA") {
-			Debug.Log ("Attack MonsterA");
+	//		Debug.Log ("Attack MonsterA");
 			MonsterAHealth monsterAHealth = gameObj.GetComponent<MonsterAHealth> ();
 			if (monsterAHealth != null && monsterAHealth.health > 0) {
 				animator.SetTrigger ("Use");//tell mecanim to do the attack animation(trigger)
@@ -286,11 +416,18 @@ public class CharacterDemoController : MonoBehaviour
 	public bool isDead(){
 		if (HP <= 0) {
 //		if(Input.GetMouseButton (2)){
-//			animator.SetInteger ("Death", 1);
+			Invoke("animation_dead",0.8f);
 			return true;
 		}
 		else
 			return false;
+	}
+	void animation_pain(){
+		animator.SetTrigger("Pain");
+	}
+
+	void animation_dead(){
+		animator.SetInteger ("Death", 1);
 	}
 
 	public void takeDamage(int damage){
@@ -298,9 +435,9 @@ public class CharacterDemoController : MonoBehaviour
 //			animator.SetTrigger ("Pain");
 //		}
 //		damaged = true;
-		animator.SetTrigger ("Pain");
+		Invoke("animation_pain",0.5f); 
 		HP -= damage;
-
+		isDead ();
 		if (HP < 0)
 			HP = 0;
 	}
@@ -321,11 +458,11 @@ public class CharacterDemoController : MonoBehaviour
 		} else if (XP >= levelXP [3] && XP < levelXP [4]) {
 			level = 4;
 		}
-		maxHP = levelHP [level];
-		attackDamage = levelDamage [level];	
+//		maxHP = levelHP [level];
+//		attackDamage = levelDamage [level];	
 	}
 
-	public void changeState(){
+/*	public void changeState(){
 		if (WeaponState == 1) {
 			maxHP = levelHP [level];
 			attackDamage = levelDamage [level];	
@@ -347,5 +484,5 @@ public class CharacterDemoController : MonoBehaviour
 			if (HP > maxHP)
 				HP = maxHP;
 		}
-	}
+	} */
 }
