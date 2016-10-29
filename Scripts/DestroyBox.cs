404c253c7375e8e3a1171e2fb9bc2660404b64d5
Vico <kongwk@foxmail.com>
2016-10-29 16:15:20 +0800

    Add files via upload

---
+++ Scripts/DestroyBox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBox : MonoBehaviour {
+
+	public void selfDestroy(){
+		Destroy (gameObject, 3.5f);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
