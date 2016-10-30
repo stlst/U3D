6b38f5b577928cfe51900caaf63c3e7a93393fa7
zhangyn944 <zhangyn94@gmail.com>
2016-10-30 12:27:49 +0800

    Add files via upload

---
+++ monster_new_script/MonsterATrace.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterATrace : MonoBehaviour {
+	public GameObject target;		//敌人的追踪目标
+	public float moveSpeed=8.0f;	//敌人的移动速度
+	public float minDist=2.5f;		//追踪距离，当敌人与目标的距离小于等于该值时，敌人不再追踪目标
+
+	private float dist;				
+	private Animator animator;				//敌人的Animator组件
+	private MonsterAHealth monsterAHealth;		//敌人的生命值脚本
+
+	public int TraceRange = 10;
+
+	//初始化，获取敌人的组件
+	void Start () {
+		animator = GetComponent<Animator> ();		//获取敌人的Animator组件	
+		monsterAHealth = GetComponent<MonsterAHealth> (); //获取敌人的生命值脚本
+		target = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	//每帧执行一次，用于敌人追踪目标
+	void Update () {
+		if (monsterAHealth!=null && monsterAHealth.health <= 0) return;	//当敌人死亡时，敌人无法追踪目标
+		if (target == null) {					//当追踪目标未设置时，敌人无法追踪目标
+			animator.SetBool ("isWalk", false);	//设置动画参数，将布尔型参数isStop设为true：敌人未追踪目标，播放停驻动画
+			return;
+		}
+		dist = Vector3.Distance (transform.position, target.transform.position);	//计算敌人与追踪目标之间的距离
+		//当游戏状态为游戏进行中（Playing）时
+		if (GameManager.gameManager==null || GameManager.gameManager.gameState == GameManager.GameState.Playing) {			
+			if (dist > minDist && dist < TraceRange) {	//当敌人与目标的距离大于追踪距离时
+	//			Debug.Log("<TraceRange");
+				transform.LookAt (target.transform);				//敌人面向追踪目标
+				transform.eulerAngles=new Vector3(0.0f,transform.eulerAngles.y,0.0f);	//设置敌人的Rotation属性，确保敌人只在y轴旋转
+				transform.position += 
+					transform.forward * moveSpeed * Time.deltaTime;	//敌人以moveSpeed的速度向追踪目标靠
+				animator.SetBool("isWalk", true);
+
+			}
+			if (dist >= TraceRange) {
+//				Debug.Log(">TraceRange");
+				animator.SetBool ("isWalk", false);
+			}
+		}
+	}
+}
---
+++ monster_new_script/MonsterBHealth.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBHealth : MonoBehaviour {
+
+	public int health=2;	//敌人的生命值
+	public int value=1;		//玩家击杀敌人后所获得的分数
+	public AudioClip monsterBHurtAudio;	//敌人的受伤音效
+	private float hittimer;
+
+	private Animator animator;			//敌人的Animator组件
+	private Collider collider1;			//敌人的Collider组件
+	private Rigidbody rigidbody1;		//敌人的rigidbody组件
+
+
+	//初始化，获取敌人的组件
+	void Start(){
+		hittimer = 0.0f;
+		animator = GetComponent<Animator> ();	//获取敌人的Animator组件
+		collider1 = GetComponent<Collider> ();	//获取敌人的Collider组件
+		rigidbody1 = GetComponent<Rigidbody> ();	//获取敌人的Rigidbody组件
+
+	}
+
+	//敌人受伤函数，用于PlayerAttack脚本中调用
+	public void TakeDamage(int damage){	
+		hittimer = 0.0f;
+		health -= damage;
+		animator.SetBool ("isHit", true);
+
+		//敌人受伤扣血
+		if (monsterBHurtAudio != null)	//在敌人位置处播放敌人受伤音效
+			AudioSource.PlayClipAtPoint (monsterBHurtAudio, transform.position);
+
+		if (health <= 0) {			//当敌人生命值小于等于0时，表明敌人已死亡
+			if (GameManager.gameManager != null) {	
+				GameManager.gameManager.player.addXP(value);//玩家获得击杀敌人后得分
+			}
+			animator.applyRootMotion = true;	//设置Animator组件的ApplyRootMotion属性，使敌人的移动与位移受动画的影响
+			animator.SetTrigger ("isDead");		//设置动画参数，设置isDead的Trigger参数，播放敌人死亡动画
+			collider1.enabled = false;			//禁用敌人的collider组件，使其不会与其他物体发生碰撞
+			rigidbody1.useGravity = false;		//因为敌人的collider组件被禁用，敌人会因重力穿过地形系统下落，取消敌人受到的重力可以避免该现象
+			Destroy (gameObject, 3.0f);			//3秒后删除敌人对象
+		}
+	}
+
+	void Update(){
+		hittimer += Time.deltaTime;
+		if (hittimer >= 1.0f) {
+			animator.SetBool ("isHit", false);
+		}
+	}
+}
